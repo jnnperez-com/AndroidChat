adf419907708454e78ed4f9016ecd267efba6e27
JONATAN PEREZ DEL ANGEL <jonatanperezsoy@gmail.com>
2016-07-23 16:25:05 -0500

    Configurar  ButterKnife
    Es para evitar el uso de find ById

--- app/src/main/java/soy/jperez/android/androidchat/LoginActivity.java
+++ app/src/main/java/soy/jperez/android/androidchat/LoginActivity.java
@@ -3,11 +3,16 @@ package soy.jperez.android.androidchat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import butterknife.ButterKnife;
+
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        ButterKnife.bind(this);
+        
     }
 }
